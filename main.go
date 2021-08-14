77066dfc01bf02de0d41abdef1fa87ba2ace340a
Alanburhannudin <70733104+Alanburhannudin@users.noreply.github.com>
2021-08-14 09:09:51 +0700

    Perbaikan Function

--- main.go
+++ main.go
@@ -10,44 +10,55 @@ func main() {
 	siswa["Reza"] = 83
 	siswa["rahma"] = 69
 
-	getMaxNumber(siswa)
-	averageNumber(siswa)
-	getMinNumber(siswa)
+	getMaxMinAveNumber(siswa)
+
 }
 
-func getMaxNumber(student map[string]float64) {
+func getMaxMinAveNumber(student map[string]float64) {
 	var maxNum float64
+	var minNum float64
+	var sum float64
 	var namaSiswa string
 	for key, value := range student {
 		if value > maxNum {
 			maxNum = value
 			namaSiswa = key
-		}
-	}
-	fmt.Println("Nilai Tertinggi Adalah ", maxNum, " yang diperoleh Oleh ", namaSiswa)
-}
-
-func averageNumber(student map[string]float64) {
-	var sum float64
-	for _, value := range student {
-		sum += value
-	}
-	ave := sum / float64(len(student))
-	fmt.Println("Nilai rata-rata siswa adalah", ave)
-}
-
-func getMinNumber(student map[string]float64) {
-	var minNum float64
-	var namaSiswa string
-	for key, value := range student {
-		if key == "Henry" {
-			minNum = value
-		}
-		if value < minNum {
+		} else {
 			minNum = value
 			namaSiswa = key
 		}
 
+		sum += value
+
 	}
+	ave := sum / float64(len(student))
+
+	fmt.Println("Nilai Tertinggi Adalah ", maxNum, " yang diperoleh Oleh ", namaSiswa)
+	fmt.Println("Nilai rata-rata siswa adalah", ave)
 	fmt.Println("Nilai Terendah Adalah ", minNum, " yang diperoleh Oleh ", namaSiswa)
 }
+
+// func averageNumber(student map[string]float64) {
+// 	var sum float64
+// 	for _, value := range student {
+// 		sum += value
+// 	}
+// 	ave := sum / float64(len(student))
+// 	fmt.Println("Nilai rata-rata siswa adalah", ave)
+// }
+
+// func getMinNumber(student map[string]float64) {
+// 	var minNum float64
+// 	var namaSiswa string
+// 	for key, value := range student {
+// 		if key == "Henry" {
+// 			minNum = value
+// 		}
+// 		if value < minNum {
+// 			minNum = value
+// 			namaSiswa = key
+// 		}
+
+// 	}
+// 	fmt.Println("Nilai Terendah Adalah ", minNum, " yang diperoleh Oleh ", namaSiswa)
+// }
